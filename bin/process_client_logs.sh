fa2257beccef953ef019075807f7874f3bc841f9
reuterShawn <reutershawn@LAPTOP-963ETN0S.localdomain>
2020-09-02 19:53:05 -0500

    Create executable process_client_logs.sh
    Created executable process_client_logs.sh and added several comments to
    help in future development of process_client_logs

---
+++ bin/process_client_logs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script takes a directory as a argument. This directory is assumed
+# to contain the un-tarred and un-gzipped log files for a single client.
+# Then the script will process the log files and generate an intermediate file
+# called failed_login_data.txt
+
+  directory=$1
+  
+  cd "$directory"
+
+# Gather contents of all the log files in current directory using cat
+
+
+# Extract the appropriate columns from the relevant lines, piping them
+# to a command that removes the minutes and seconds from all the times
+
+
+# Redirect the output to the file failed_login_data.txt
