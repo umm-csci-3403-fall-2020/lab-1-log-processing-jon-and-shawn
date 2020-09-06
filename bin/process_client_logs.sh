c5f395001e3bedc650799b64ee8482a991b6fd10
Shawn <reute078@morris.umn.edu>
2020-09-06 13:00:42 -0500

    Fix process_client_logs.sh tests
    Fixed tests by cd into the temp directory intstead of putting the
    directory directly in the path.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -7,11 +7,9 @@
 
   directory=$1
 
-# Gather contents of all the log files in current directory using cat
-
-# Extract the appropriate columns from the relevant lines, piping them
-# to a command that removes the minutes and seconds from all the times
+# Move into temp directory
+  cd "$directory" || exit
 
 
-# Redirect the output to the file failed_login_data.txt
-cat "$directory"/var/log/* | awk '/Failed password/ {print $1,$2,$3,$9,$11,$13}' | sed -E 's/:\w+:\w+//' | sed -E 's/ invalid| [0-9]{4,5}//' > failed_login_data.txt
+# Combine all the files together and then filter out specific data with awk
+  cat var/log/* | awk '/Failed password/ {print $1,$2,$3,$9,$11,$13}' | sed -E 's/:\w+:\w+//' | sed -E 's/ invalid| [0-9]{4,5}//' > failed_login_data.txt
