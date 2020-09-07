8aea405af09d86b5c049562326b53fdcd748aa43
JReuvers <reuve026@morris.umn.edu>
2020-09-06 20:35:16 -0500

    Make create_username_dist.sh pass its tests
    Also fixed some shellcheck problems with process_logs.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,6 +2,11 @@
 
   directory=$1
 
+  here=`pwd`
+
   cd "$directory" || exit
 
 # Gather the contents of the failed_login_data.txt file and extract the username columns.                               # Then count how many times each user name appears 
+sort -k 4 */failed_login_data.txt | awk '{print $4}' | uniq -c | awk '{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' | sort > tempUsername.txt
+
+"$here"/bin/wrap_contents.sh tempUsername.txt "$here"/html_components/username_dist username_dist.html
