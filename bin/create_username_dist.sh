e7c5dce9402075bb40b8a8385fb0035229c34c14
Shawn <reute078@morris.umn.edu>
2020-09-13 17:35:21 -0500

    Add specific comments to create_username_dist.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -6,10 +6,12 @@
 
   cd "$directory" || exit
 
-# Gather the contents of the failed_login_data.txt file and extract the username columns.                               # Then count how many times each user name appears 
-  sort -k 4 -- */failed_login_data.txt |
-  awk '{print $4}' | 
-  uniq -c |
-  awk '{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' | sort |
+# Gather the contents of the failed_login_data.txt file and extract the username columns.                               
+  sort -k 4 -- */failed_login_data.txt | awk '{print $4}' | 
+
+# Use uniq to count number of occurences of each username
+# Then use awk to print each username with number of occurences afterwards  
+  uniq -c | awk '{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' | sort |
+
 # Wrap contents and create username_dist.html file
   "$here"/bin/wrap_contents.sh - "$here"/html_components/username_dist username_dist.html
