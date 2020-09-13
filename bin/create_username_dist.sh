b28b175a5a9b4cda5dec9f86bcf981b9fcf55d08
JReuvers <reuve026@morris.umn.edu>
2020-09-13 16:31:41 -0500

    Remove creation of intermediate file

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -10,7 +10,6 @@
   sort -k 4 -- */failed_login_data.txt |
   awk '{print $4}' | 
   uniq -c |
-  awk '{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' | sort > tempUsername.txt
-
+  awk '{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' | sort |
 # Wrap contents and create username_dist.html file
-  "$here"/bin/wrap_contents.sh tempUsername.txt "$here"/html_components/username_dist username_dist.html
+  "$here"/bin/wrap_contents.sh - "$here"/html_components/username_dist username_dist.html
