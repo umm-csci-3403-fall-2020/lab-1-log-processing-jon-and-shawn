2dc953849942f7a4b299f49ec73b737f1bfd30f9
Shawn <reute078@morris.umn.edu>
2020-09-13 16:26:20 -0500

    Finish functionality for create_country_dist.html

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
+  here=$(pwd)
 
-# Extract IP addresses and use join to map them to countries
+  directory=$1
 
-# Then count occurences of each country using uniq -c
+  cd "$directory" || exit
 
-# Print rows using data.addrow:
+# Sort country_IP_map.txt file to use later
+  sort "$here"/etc/country_IP_map.txt
+
+# Extract IP addresses and sort them  
+  sort -k 5 -- */failed_login_data.txt | awk '{print $5}' | sort |
+
+# Join two files together using goin then print the second column which is the country 
+  join -1 1 -2 1 - "$here"/etc/country_IP_map.txt | awk '{print $2}' | sort |
+
+# Use uniq to count occurances of country names then using awk print the country followed by occurences of that country
+  uniq -c | awk '{print "data.addRow([\x27" $2 "\x27, "$1"]);"}' |
+
+# Call execuatable wrap_contents.sh to wrap take in a file through the pipe and country_dist header and footer
+# Finally name the finished wrap file country_dist.html
+  "$here"/bin/wrap_contents.sh - "$here"/html_components/country_dist country_dist.html
