eea958f6a6093e20efaa885926b31fcf35060ae5
JReuvers <reuve026@morris.umn.edu>
2020-09-13 18:14:27 -0500

    Make process_logs.sh pass test

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -2,9 +2,24 @@
 
 # Takes a list of gzipped tar files from the command line
 
-tempDir=$(mktemp -d)
+  here=$(pwd)
 
-for FILE in $1 $2 $3 $4 $5 $6
-do
-	tar -xvf "$FILE" -C "$tempDir"
-done
+  tempDir=$(mktemp -d)
+
+  for FILE in "$@"
+  do
+	basename=$(basename -s .tgz "$FILE")
+	dirName=$(echo "$basename" | awk -F '_' '{print $1}')
+	mkdir "$tempDir"/"$dirName"
+
+	tar -xvf "$FILE" -C "$tempDir"/"$dirName"
+	bin/process_client_logs.sh "$tempDir"/"$dirName"
+  done
+	cd "$here" || exit
+
+	bin/create_username_dist.sh "$tempDir" 
+	bin/create_hours_dist.sh "$tempDir" 
+	bin/create_country_dist.sh "$tempDir" 
+	bin/assemble_report.sh "$tempDir" 
+
+	mv "$tempDir"/failed_login_summary.html "$here"
